550e34d230d8be28b6a4a50950732000e2810808
99002640 <subin.suthan@ltts.com>
2020-12-14 10:13:54 +0530

    added

---
+++ LogApi/Controllers/DevicevalueController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+//using System.Web.Http;
+//using System.Web.Mvc;
+using LogApi.VM;
+
+namespace LogApi.Controllers
+{
+    public class DevicevalueController : ApiController
+    {
+
+        //    [RoutePrefix("Api/DeviceDetail")]
+        //    // GET: Devicevalue
+        //    public ActionResult Index()
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
